aecc84d1296eb470de3a390666c98a0913a73c82
Israel Leiva <leiva.israel@gmail.com>
2013-02-05 01:21:24 -0300

    Comentario arreglado

--- coj/1179.cpp
+++ coj/1179.cpp
@@ -1,7 +1,6 @@
 /* Caribbean Online Judge
  * Problema: 1179 - Optimal Parking
  * Categoria: Ad hoc
- * 
  * Israel Leiva - 15-11-2011
  */
 #include <iostream>
@@ -12,6 +11,7 @@ using namespace std;
  * hasta la que esta mas lejos, desde ahi visitarlas todas y luego 
  * devolverse.
  */
+
 int main(){
 	int T, N, i, stores[21], dist;
 	cin >> T;
@@ -19,7 +19,7 @@ int main(){
 		cin >> N;
 		for(i=0;i<N;i++)
 			cin >> stores[i]; 
-		
+
 		sort(stores, stores+N);
 		dist=0;
 		for(i=N-2;i>=0;i--)
